ff816469a42b17dcaa964a1bb09878e253ca42bf
Juan Miguel <juanmiber@amazon.com>
2023-07-12 15:44:43 -0500

    Se agregan funciones lambdas al proyecto

---
+++ test/test_funciones.py
@@ -0,0 +1,15 @@
+import pytest
+import json
+
+from funciones import saludador
+
+
+def test_lambda_status_code(): 
+    res = saludador.handler({}, {})
+    assert res.get('statusCode') == 200
+    expected = {
+        "statusCode": 200,
+        "body": "Hola desde lambda!"
+    }
+    assert expected == res
+
